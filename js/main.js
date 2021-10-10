0242eaecf4105979e692b1dbd78b0b4750517b59
dinhmanh245xz <61041412+dinhmanh245xz@users.noreply.github.com>
2021-10-10 20:27:52 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(".menu-items > li").hover(function() {
+    $(".header-menu-bg").addClass('active');
+}).mouseleave(function() {
+    
+   
+    setTimeout(function() {
+       
+
+    }, 1000);
+
+});
+
+$(".menu-items").mouseleave(function(){
+    $(".header-menu-bg").removeClass('active');
+});
+$(".dropdown-menu").mouseleave(function(){
+    $(".header-search__category").removeClass('open');
+});
+
+$(".dropdown-menu").mouseleave(function(){
+    $(".header-language").removeClass('open');
+});
+
+const opendropdown = document.querySelector(".header-search__category");
+opendropdown.addEventListener('click',(event) => {
+    opendropdown.classList.toggle('open');
+})
+
+
+const openlanguage = document.querySelector(".header-language");
+openlanguage.addEventListener('click',(event) => {
+    openlanguage.classList.toggle('open');
+})
+
+const openAccount = document.querySelector(".header__account");
+openAccount.addEventListener('click',(event) => {
+    openAccount.classList.toggle('open');
+})
+
+function changeSpan (element) {
+    let tags = document.getElementsByClassName("lbl-btn");
+    for(i = 0; i < tags.length; i++){
+        tags[i].style.background="#fff ";
+        tags[i].style.color="#000";   
+    }
+    element.style.background="#000";
+    element.style.color="#fff";
+    
+}
+
+let thumb = document.querySelectorAll(".thumbs");
+Array.from(thumb).forEach((img)=>{
+    img.addEventListener('click', function () {
+        let srcImg = this.getAttribute('src');
+        document.querySelector('#thumb-main').setAttribute('src',srcImg);
+    })
+})
+
+
+
+
+
+
+
+// document.querySelector(".menu-items > li").hover(function(){
+//     document.querySelector(".header-menu-bg").addClass("active");
+
+// }).mouseleave(function(){
+//     setTimeout(function(){
+
+//     },1000);
+// });
+
